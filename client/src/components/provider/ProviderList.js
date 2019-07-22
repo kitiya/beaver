28609a93b09a9e116754a2e081df9defafb3e4d0
Kitiya <kate.kitiya@gmail.com>
2019-07-22 05:23:56 -0600

    Refactor: fetching providers

--- client/src/components/provider/ProviderList.js
+++ client/src/components/provider/ProviderList.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 
-export default function ProviderList() {
-    const [data, setData] = useState({providers: []});
+const useFetch = (initialState, url) => {
+    const [data, setData] = useState(initialState);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch('http://localhost:8080/api/providers');
+                const response = await fetch(url);
                 const result = await response.json();
                 setData({providers: result});
             } catch (error) {
@@ -14,26 +15,38 @@ export default function ProviderList() {
             }
         };
         fetchData();
-    }, []);
+        setLoading(false);
+    }, [url]);
+    return { data, loading };
+}
+
+export default function ProviderList() {
+    const { data, loading } = useFetch({providers: []}, "http://localhost:8080/api/providers");
 
     return (
         <div className="container mt-3">
-            {data.providers.map((provider) => (
-                <section key={provider.id}>
-                    <h3 className="text-info">{provider.name}</h3>
-                    <div className="row mb-3">
-                        {provider.imageUrls.map((url, index) => (
-                            <div className="col-4" key={index}>
-                                <img className="card-img-top rounded border" src={url} alt={provider.name} />
+            {   loading ?
+
+                <p>...loading</p> :
+
+                <div className="row">
+                    {data.providers.map((provider) => (
+                        <section key={provider.id}>
+                            <h3 className="bg-light text-info text-center rounded py-1 border border-bottom-0 border-right-0 border-left-0">{provider.name}</h3>
+                            <div className="row mb-3">
+                                {provider.imageUrls.map((url, index) => (
+                                    <div className="col-4" key={index}>
+                                        <img className="card-img-top rounded border" src={url} alt={provider.name} />
+                                    </div>
+                                ))}
                             </div>
-                        ))}
-                    </div>
-                    <p>{provider.description}</p>
-                    <p>Location: {provider.location}</p>
-                    <p>Website: {provider.website}</p>
-                    <hr />
-                </section>
-            ))}
+                            <p>{provider.description}</p>
+                            <p>Location: {provider.location}</p>
+                            <p>Website: {provider.website}</p>
+                        </section>
+                    ))}
+                </div>
+            }
         </div>
     );
 };
